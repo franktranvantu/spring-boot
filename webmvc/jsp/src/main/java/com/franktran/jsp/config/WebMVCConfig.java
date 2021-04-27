eb6da57e84ea7df6a3c2c51f58e0cbe0e70546a3
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-04-27 18:12:19 +0700

    Update

---
+++ webmvc/jsp/src/main/java/com/franktran/jsp/config/WebMVCConfig.java
@@ -0,0 +1,21 @@
+package com.franktran.jsp.config;
+
+import com.franktran.jsp.formatter.DateFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer {
+
+    private final DateFormatter dateFormatter;
+
+    public WebMVCConfig(DateFormatter dateFormatter) {
+        this.dateFormatter = dateFormatter;
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(dateFormatter);
+    }
+}
